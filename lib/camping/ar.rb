366ead671e0e59c7cbb9485da8da57247a4db799
Sean Busbey <sean.busbey@gmail.com>
2010-03-27 12:49:10 +0000

    cleaning up use of whitespace

--- lib/camping/ar.rb
+++ lib/camping/ar.rb
@@ -8,6 +8,7 @@ end
 
 $AR_EXTRAS = %{
   Base = ActiveRecord::Base unless const_defined? :Base
+
   class SchemaInfo < Base
   end
 
@@ -19,7 +20,7 @@ $AR_EXTRAS = %{
       meta_def(:inherited) { |k| m << k }
     end
   end
-  
+
   def self.create_schema(opts = {})
     opts[:assume] ||= -2
     opts[:version] ||= @final
@@ -48,41 +49,41 @@ $AR_EXTRAS = %{
   \# and then define a derivative of Base on the fly.  We can then tie the model that
   \# inherits our singleton to the block of code the user gave for table creation.
   \# Presto! a migration pops into existence for each model we create.
-  \# 
+  \#
   \# So long as new models are added after existing models, we should correctly keep
   \# adding them in.
-  \# 
+  \#
   \# When you finally need a non-destructive table change adding a migration with a positive
   \# number will run as expected, but will preclude further automatic table creation. :(
   def self.Base(opts={}, &block)
     @final = -2 if @final.nil?	
-	v = V -1.0/(1+(@migrations ||= []).size)
-    Class.new(Base) do  
-	  @abstract_class = true
-	  @V = v
-	  meta_def(:inherited) do |model|
-	    Class.new(@V) do
-		  @model = model; @opts = opts; @block = block
+    v = V -1.0/(1+(@migrations ||= []).size)
+    Class.new(Base) do
+      @abstract_class = true
+      @V = v
+      meta_def(:inherited) do |model|
+        Class.new(@V) do
+          @model = model; @opts = opts; @block = block
           def self.up
             q = []
             later = Proc.new do |attributes, &b|
               q << [attributes, b]
             end
-			create_table @model.table_name, @opts do |t|
-			  (class << t; self; end).class_eval do define_method(:create, &later); end
-			  @block.call t
-			end
+            create_table @model.table_name, @opts do |t|
+              (class << t; self; end).class_eval do define_method(:create, &later); end
+              @block.call t
+            end
             q.each do |attributes, b|
               @model.create attributes, &b
-			end
-		  end
-		  def self.down
-		    drop_table @model.table_name
-		  end
-		end
-		super
-	  end
-	end
+            end
+          end
+          def self.down
+            drop_table @model.table_name
+          end
+        end
+        super
+      end
+    end
   end
 }
 
@@ -105,7 +106,7 @@ module Camping
     # ActiveRecord is not loaded if you never reference this class.  The minute you
     # use the ActiveRecord or Camping::Models::Base class, then the ActiveRecord library
     # is loaded.
-	Base = A::Base
+    Base = A::Base
 
     # The default prefix for Camping model classes is the topmost module name lowercase
     # and followed with an underscore.
